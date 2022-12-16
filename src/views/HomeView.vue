18d34475577d58e1aaa2bd61dc6da53e940f2948
Raphael <raphaelsfreitas94@gmail.com>
2022-12-15 22:25:13 -0300

    add bootstrap and login

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,31 @@
-<script setup>
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <body>
+    <div class="login">
+      <form>
+        <h1>Entrar</h1>
+        <div class="mb-3">
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Endereço de email"
+          />
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label"></label>
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+            placeholder="Senha"
+          />
+        </div>
+        <a href="http://">Esqueci minha senha</a>
+        <button type="submit" class="btn btn-primary">Entrar</button>
+      </form>
+    </div>
+  </body>
 </template>
